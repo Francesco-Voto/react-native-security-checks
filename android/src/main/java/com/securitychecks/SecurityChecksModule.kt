b6fe76347803859f162bad3beefe880cfd610d8a
Francesco-Voto <francesco.voto@nearform.com>
2025-02-17 16:58:29 +0100

    feat: change isFridaDetected to avoid false positive on android

--- android/src/main/java/com/securitychecks/SecurityChecksModule.kt
+++ android/src/main/java/com/securitychecks/SecurityChecksModule.kt
@@ -5,32 +5,33 @@ import com.facebook.react.module.annotations.ReactModule
 
 @ReactModule(name = SecurityChecksModule.NAME)
 class SecurityChecksModule(reactContext: ReactApplicationContext) :
-  NativeSecurityChecksSpec(reactContext) {
+    NativeSecurityChecksSpec(reactContext) {
 
     private var frida: FridaSecurityChecker = FridaSecurityChecker()
 
-  override fun getName(): String {
-    return NAME
-  }
+    override fun getName(): String {
+        return NAME
+    }
 
-  override fun isFridaDetected(): Boolean {
+    override fun isFridaDetected(): Boolean {
+        return listOf(
+            frida.isDetectedDevKeys,
+            frida.isDetectedTestKeys,
+            frida.isSuExists,
+            frida.isFoundHooks,
+            frida.isFoundXposed,
+            frida.isFoundSuBinary,
+            frida.isPermissiveSelinux,
+            frida.isFoundResetProp,
+            frida.isAccessedSuperuserApk,
+            frida.isFoundDangerousProps,
+            frida.isNotFoundReleaseKeys,
+            frida.isFoundWrongPathPermission,
+            frida.isFoundBusyboxBinary
+        ).count { it } > 2
+    }
 
-      return frida.isDetectedDevKeys ||
-        frida.isDetectedTestKeys ||
-        frida.isSuExists ||
-        frida.isFoundHooks ||
-        frida.isFoundXposed ||
-        frida.isFoundSuBinary ||
-        frida.isPermissiveSelinux ||
-        frida.isFoundResetProp ||
-        frida.isAccessedSuperuserApk ||
-        frida.isFoundDangerousProps ||
-        frida.isNotFoundReleaseKeys ||
-        frida.isFoundWrongPathPermission ||
-        frida.isFoundBusyboxBinary
-  }
-
-  companion object {
-    const val NAME = "SecurityChecks"
-  }
+    companion object {
+        const val NAME = "SecurityChecks"
+    }
 }
