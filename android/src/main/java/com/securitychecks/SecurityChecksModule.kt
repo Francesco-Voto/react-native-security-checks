8cfb769fab543f2310e36cd87931f5e4b465f08e
Francesco-Voto <francesco.voto@nearform.com>
2025-02-20 15:49:41 +0100

    feat: add a better check for android emulator

--- android/src/main/java/com/securitychecks/SecurityChecksModule.kt
+++ android/src/main/java/com/securitychecks/SecurityChecksModule.kt
@@ -8,9 +8,29 @@ class SecurityChecksModule(reactContext: ReactApplicationContext) :
     NativeSecurityChecksSpec(reactContext) {
 
     private var frida: FridaSecurityChecker = FridaSecurityChecker()
+    private var numberOfChecks: Int = if (checkBuildConfig()) 3 else 2
 
     override fun getName(): String {
         return NAME
+    }
+
+    private fun checkBuildConfig(): Boolean {
+        return (Build.MANUFACTURER.contains("Genymotion")
+                || Build.MODEL.contains("google_sdk")
+                || Build.MODEL.toLowerCase().contains("droid4x")
+                || Build.MODEL.contains("Emulator")
+                || Build.MODEL.contains("Android SDK built for x86")
+                || Build.HARDWARE == "goldfish"
+                || Build.HARDWARE == "vbox86"
+                || Build.HARDWARE.toLowerCase().contains("nox")
+                || Build.FINGERPRINT.startsWith("generic")
+                || Build.PRODUCT == "sdk"
+                || Build.PRODUCT == "google_sdk"
+                || Build.PRODUCT == "sdk_x86"
+                || Build.PRODUCT == "vbox86p"
+                || Build.PRODUCT.toLowerCase().contains("nox")
+                || Build.BOARD.toLowerCase().contains("nox")
+                || (Build.BRAND.startsWith("generic") &&    Build.DEVICE.startsWith("generic")))
     }
 
     override fun isFridaDetected(): Boolean {
@@ -28,7 +48,7 @@ class SecurityChecksModule(reactContext: ReactApplicationContext) :
             frida.isNotFoundReleaseKeys,
             frida.isFoundWrongPathPermission,
             frida.isFoundBusyboxBinary
-        ).count { it } > 2
+        ).count { it } > numberOfChecks
     }
 
     companion object {
