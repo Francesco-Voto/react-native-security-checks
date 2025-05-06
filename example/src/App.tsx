7c3977af4514e405b75069be4fc3fce4ba4187dc
Francesco-Voto <francesco.voto@nearform.com>
2025-05-06 17:02:43 +0200

    feat: add cheeck on signature for android

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,10 +1,21 @@
 import { StyleSheet, View, Text } from 'react-native';
-import { isFridaDetected } from 'react-native-security-checks';
+import {
+  isFridaDetected,
+  isSignatureCompromised,
+} from 'react-native-security-checks';
+
+const SIGNATURE =
+  'fac61745dc0903786fb9ede62a962b399f7348f0bb6f899b8332667591033b9c';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Frida detected: {isFridaDetected() ? 'yes' : 'no'}</Text>
+
+      <Text>
+        Signature comrpomised:{' '}
+        {isSignatureCompromised(SIGNATURE) ? 'yes' : 'no'}
+      </Text>
     </View>
   );
 }
